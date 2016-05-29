4de5bcb0312a6b9e93cf4bbce8721e1fc0b9ac9f
e_Oliveira <e_oliveira12@outlook.com>
2016-05-29 15:04:51 -0300

    Replace sonar token

--- api.py
+++ api.py
@@ -3,21 +3,21 @@ from models import SlackApi
 from flask import jsonify
 
 app = Flask(__name__)
-slack_api = SlackApi("xoxp-2195292530-15854845845-42615916823-217a7f4771")
+slack_api = SlackApi("REPLACE-IT-WITH-YOUR-TOKEN")
 
-@app.route("/nsk/slack/users/list")
+@app.route("/app/slack/users/list")
 def users_list():
     return jsonify(slack_api.users_list())
 
-@app.route("/nsk/slack/channels/list")
+@app.route("/app/slack/channels/list")
 def channels_list():
     return jsonify(slack_api.channels_list())
 
-@app.route("/nsk/slack/find/<channel_id>/<count>")
+@app.route("/app/slack/find/<channel_id>/<count>")
 def find_messages(channel_id, count):
     return jsonify(slack_api.find_messages(channel_id, count))
 
-@app.route("/nsk/slack/post/message/<message>/<channel>", methods = ['POST'])
+@app.route("/app/slack/post/message/<message>/<channel>", methods = ['POST'])
 def post_message(message, channel):
     return jsonify(slack_api.post_message(message=message, channel=channel))
 
